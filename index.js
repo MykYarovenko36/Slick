cbc5365315d1d342c7b8b5ae7b512f326cbb9923
N_Yarovenko <mykolayarovenko@gmail.com>
2021-01-14 04:09:50 +0200

    slick-test

---
+++ index.js
@@ -0,0 +1,33 @@
+$(document).ready(() => {
+    $('.slider').slick({
+        dots: true,
+        infinity: true,
+        slidesToShow: 3,
+        centerMode: true,
+        arrows: true,
+        rows: 2,
+        slidesPerRow: 1,
+        prevArrow: 
+          '<button type="button" class="far fa-angle-left slick-prev">Previous</button>',
+        nextArrow: 
+          '<button type="button" class="far fa-angle-right slick-next">Next</button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    rows: 1,
+                    arrows: false,
+                }
+            },
+        ]
+    });
+});
